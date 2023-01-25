22423f52a542b8e1c5070ff96dddea88b1dc9e27
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-01-25 15:35:56 +0000

    test: add TestDirsWithFunc

--- setuptest/dir_test.go
+++ setuptest/dir_test.go
@@ -1,6 +1,8 @@
 package setuptest
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,4 +13,20 @@ func TestDirs(t *testing.T) {
 
 	test := Dirs("testdata/depth1", "").WithVars(map[string]interface{}{}).InitPlanShow(t)
 	require.NoError(t, test.Err)
+}
+
+func TestDirsWithFunc(t *testing.T) {
+	t.Parallel()
+
+	var f SetupTestPrepFunc = func(resp SetupTestResponse) error {
+		f, err := os.Create(filepath.Join(resp.TmpDir, "test.txt"))
+		if err != nil {
+			return err
+		}
+		return f.Close()
+	}
+
+	test := Dirs("testdata/depth1", "").WithVars(map[string]interface{}{}).InitPlanShowWithPrepFunc(t, f)
+	require.NoError(t, test.Err)
+	require.FileExists(t, filepath.Join(test.TmpDir, "test.txt"))
 }
